5315968745c299ba64935d6548820aaee2ecb2c4
jarred barber <jpb5082@gmail.com>
2019-07-08 22:43:10 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // Draw some multi-colored geometry to the screen
 extern crate quicksilver;
 extern crate rand;
+extern crate vector2d;
 extern crate specs;
 #[macro_use]
 extern crate specs_derive;
@@ -15,6 +16,8 @@ use quicksilver::{
     lifecycle
 };
 
+type V2 = vector2d::Vector2D<f32>;
+
 /*
  * Components
  */
@@ -24,6 +27,12 @@ struct Player {
     health: u32,
     // last_bullet: u32,
 }
+
+#[derive(Debug, Component)]
+struct Asteroid;
+
+#[derive(Debug, Component)]
+struct Bullet;
 
 #[derive(Debug, Copy, Clone, Component)]
 struct RigidBody {
@@ -55,20 +64,37 @@ impl Polygon {
             A += 0.5*(x[k]*y[k+1] - x[k+1]*y[k]);
         }
 
-        let mut Cx: f32 = 0.0;
-        let mut Cy: f32 = 0.0;
+        let mut cx: f32 = 0.0;
+        let mut cy: f32 = 0.0;
 
         let scale: f32 = 6.0*A;
 
         for k in 0..(x.len() - 1) {
             let b = (x[k]*y[k+1] - x[k+1]*y[k])/scale;
-            Cx += b*(x[k] + x[k+1]);
-            Cy += b*(y[k] + y[k+1]);
+            cx += b*(x[k] + x[k+1]);
+            cy += b*(y[k] + y[k+1]);
         }
 
-        Polygon { x: x.iter().map(|&a|  (a - Cx)).collect(),
-                  y: y.iter().map(|&a|  (a - Cy)).collect() }
+        let mut p = Polygon { x: x.iter().map(|&a|  (a - cx)).collect(),
+                              y: y.iter().map(|&a|  (a - cy)).collect() };
+        p.x.push(p.x[0]);
+        p.y.push(p.y[0]);
+        p
     }
+
+    fn len(&self) -> usize {
+        self.x.len() - 1
+    }
+
+    fn area(&self) -> f32 {
+        let mut A: f32 = 0.0;
+        for k in 0..self.len() {
+            A += 0.5*(self.x[k]*self.y[k+1] 
+                      - self.x[k+1]*self.y[k]);
+        }
+        A
+    }
+
 }
 
 #[derive(Debug, Component)]
@@ -79,11 +105,10 @@ struct Color { color: graphics::Color }
  * Systems
  */
 
-// Systems
 struct PhysicsUpdate;
+struct BulletAsteroidCollision;
 struct Collision;
 
-// Resources
 
 impl<'a> specs::System<'a> for PhysicsUpdate {
     type SystemData = specs::WriteStorage<'a, RigidBody>;
@@ -111,6 +136,62 @@ impl<'a> specs::System<'a> for PhysicsUpdate {
     }
 }
 
+impl<'a> specs::System<'a> for BulletAsteroidCollision {
+    type SystemData = (
+            specs::Entities<'a>,
+            specs::ReadStorage<'a, RigidBody>,
+            specs::ReadStorage<'a, Polygon>,
+            specs::ReadStorage<'a, Asteroid>,
+            specs::ReadStorage<'a, Bullet>,
+            specs::Read<'a, specs::LazyUpdate>);
+    fn run(&mut self, (entities, rb_read, poly_read, ast_read, bul_read, update): Self::SystemData) {
+        //TODO: refactor this. this is ridiculous.
+        use specs::Join;
+        
+        for (ent_bullet, rb_bullet, bullet) in (&entities, &rb_read, &bul_read).join() {
+            let xb = rb_bullet.x;
+            let yb = rb_bullet.y;
+            for (ent_ast, rb_ast, ast_poly, ast) in (&entities, &rb_read, &poly_read, &ast_read).join() {
+                // For each line segment in the polygon,
+                // compute the distance from the bullet to the line segment
+                for k in 0..(ast_poly.x.len() - 1) {
+                    let c = rb_ast.phi.cos();
+                    let s = rb_ast.phi.sin();
+
+                    let x0 = rb_ast.x + c*ast_poly.x[k+0] - s*ast_poly.y[k+0]; 
+                    let y0 = rb_ast.y + s*ast_poly.x[k+0] + c*ast_poly.y[k+0]; 
+                    let x1 = rb_ast.x + c*ast_poly.x[k+1] - s*ast_poly.y[k+1]; 
+                    let y1 = rb_ast.y + s*ast_poly.x[k+1] + c*ast_poly.y[k+1]; 
+
+                    let l2 = (x1-x0)*(x1 - x0) + (y1 - y0)*(y1 - y0);
+                    let t = (((xb - x0)*(x1 - x0) + (yb - y0)*(y1 - y0))/l2);
+                    let xp = (1.0 - t)*x0 + t*x1;
+                    let yp = (1.0 - t)*y0 + t*y1;
+
+                    let d = (xp - xb)*(xp - xb) + (yp - yb)*(yp - yb);
+                    
+                    if (d < 0.5 && t > 0.0 && t < 1.0) {
+                        // Call this a collision
+                        entities.delete(ent_bullet);
+                        entities.delete(ent_ast);
+
+                        // Get area
+                        let A = ast_poly.area(); 
+                        if (A > 10.0) {
+                            let rng = rand::thread_rng();
+                            // Momentum
+                            let mx = rb_ast.vx*A;
+                            let my = rb_ast.vy*A;
+                            
+                            
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 /*
  * Quicksilver stuff
  */
@@ -175,6 +256,7 @@ impl GameSession<'static, 'static> {
         use specs::Builder;
         self.world.create_entity()
             .with(rb)
+            .with(Asteroid)
             .with(Polygon::new(x, y))
             .with(Color {color: graphics::Color::WHITE})
             .build();
@@ -230,6 +312,7 @@ impl GameSession<'static, 'static> {
             Some(rb) => {
                 let vb:f32 = 5.0;
                 self.world.create_entity()
+                    .with(Bullet)
                     .with(RigidBody{x: rb.x, y: rb.y,
                                     vx: rb.vx + vb*rb.phi.cos(),
                                     vy: rb.vy + vb*rb.phi.sin(), 
@@ -252,9 +335,12 @@ impl lifecycle::State for GameSession<'static, 'static> {
         world.register::<Polygon>();
         world.register::<Player>();
         world.register::<Color>();
+        world.register::<Bullet>();
+        world.register::<Asteroid>();
 
         let dispatcher = specs::DispatcherBuilder::new()
             .with(PhysicsUpdate, "physics", &[])
+            .with(BulletAsteroidCollision, "ba-collision", &[])
             .build();
         Ok(GameSession { state: GameState::Init, 
                          world: world, 
@@ -293,6 +379,7 @@ impl lifecycle::State for GameSession<'static, 'static> {
             GameState::GameOver => ()
         };
         self.dispatcher.dispatch(&mut self.world.res);
+        self.world.maintain();
         Ok(())
     }  
 
@@ -317,15 +404,11 @@ impl lifecycle::State for GameSession<'static, 'static> {
             let n:usize = poly.x.len();
             let c = rb.phi.cos();
             let s = rb.phi.sin();
-            for k in 0..n {
+            for k in 0..(n-1) {
                 let v0 = quicksilver::geom::Vector::new(rb.x + c*poly.x[k] - s*poly.y[k], 
                                                         rb.y + s*poly.x[k] + c*poly.y[k]);
-                let v1 = quicksilver::geom::Vector::new(rb.x 
-                                                        + c*poly.x[(k+1).wrapping_rem(n)]
-                                                        - s*poly.y[(k+1).wrapping_rem(n)],
-                                                        rb.y 
-                                                        + c*poly.y[(k+1).wrapping_rem(n)]
-                                                        + s*poly.x[(k+1).wrapping_rem(n)]);
+                let v1 = quicksilver::geom::Vector::new(rb.x + c*poly.x[k+1] - s*poly.y[k+1], 
+                                                        rb.y + s*poly.x[k+1] + c*poly.y[k+1]);
                 window.draw(&geom::Line::new(v0, v1).with_thickness(0.1),
                             graphics::Background::Col(color.color));
             }
